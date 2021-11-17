7023c6b05c3de8c5ed89ea912acc8e1803fed7ca
GiantProgrammer <37694356+BombaStudio@users.noreply.github.com>
2021-11-17 14:25:22 +0300

    Update main.py
    mesaj gönderme

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print('{0.user} olarak Hünkarımızın yanına geldik'.format(client))
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    if message.content.startswith('*Merhaba'):
+        await message.channel.send('Merhaba')
 
 client.run(os.getenv('OTEwNDcwMjkyOTE5NDUxNjQ4.YZTTiQ.27Rsu-uaPL22Ww8NOlYVOMP9HCM'))
