1cc7230b1f28f3e8c6ecbb834ce90f13c6f8f4f2
GiantProgrammer <37694356+BombaStudio@users.noreply.github.com>
2021-11-17 14:27:57 +0300

    Update main.py
    Hürrem küfürleri öğreniyor

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import os
 
 client = discord.Client()
 
+kufurler = [
+    "amk","oç","aq"
+]
+
 @client.event
 async def on_ready():
     print('{0.user} olarak Hünkarımızın yanına geldik'.format(client))
@@ -14,5 +18,9 @@ async def on_message(message):
 
     if message.content.startswith('*Merhaba'):
         await message.channel.send('Merhaba')
+        
+    for i in range(0,len(kufurler)):
+        if message.content.startswith(kufurler[i]):
+            await message.channel.send('Bu ne hadsizlik !!!')
 
 client.run(os.getenv('OTEwNDcwMjkyOTE5NDUxNjQ4.YZTTiQ.27Rsu-uaPL22Ww8NOlYVOMP9HCM'))
